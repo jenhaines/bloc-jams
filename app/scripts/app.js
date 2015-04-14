958e73d54983b8858983c5b840e4a687e736684b
Jen Haines <motochick67@gmail.com>
2015-04-13 20:58:58 -0700

    Volume Control added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -107,6 +107,14 @@ blocJams.controller('Collection.controller', ['$scope', 'SongPlayer', function($
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
 
+   $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+
    SongPlayer.onTimeUpdate(function(event, time){
       $scope.$apply(function(){
         $scope.playTime = time;
@@ -126,6 +134,7 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
 
     play: function() {
       this.playing = true;
@@ -163,9 +172,16 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
          currentSoundFile.setTime(time);
        }
      },
-     onTimeUpdate: function(callback) {
-         return $rootScope.$on('sound:timeupdate', callback);
-       },
+      onTimeUpdate: function(callback) {
+        return $rootScope.$on('sound:timeupdate', callback);
+      },
+      setVolume: function(volume) {
+        if(currentSoundFile){
+          currentSoundFile.setVolume(volume);
+        }
+        this.volume = volume;
+      },
+
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -177,6 +193,8 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
         formats: [ "mp3" ],
         preload: true
       });
+
+      currentSoundFile.setVolume(this.volume);
 
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
